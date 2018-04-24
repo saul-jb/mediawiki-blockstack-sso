68d03079dda10639f6c5ed5a0eeded5830e56694
Aranad <aran@organicdesign.co.nz>
2018-04-24 13:24:57 -0300

    dodgy way to do local dapp test

--- modules/blockstacksso.js
+++ modules/blockstacksso.js
@@ -1,19 +1,22 @@
 $('#mw-input-blockstacksso').click(function() {
-	var retUrl = mw.config.get('wgServer') + mw.config.get('wgScript') + '?title=' + mw.config.get('wgPageName');
-	var manUrl = mw.config.get( 'blockstackManifestUrl' );
-	$.ajax({
-		type: 'GET',
-		url: 'http://localhost:1337',
-		timeout: 100,
-		success: function(text) {
+	var img = new Image()
+	img.src = 'http://localhost:8888/images/icon-nav-profile.svg';
+	setTimeout(function() {
+		console.log(img.height);
+
+		var retUrl = mw.config.get('wgServer') + mw.config.get('wgScript') + '?title=' + mw.config.get('wgPageName');
+		var manUrl = mw.config.get( 'blockstackManifestUrl' );
+
+		if(img.height > 0) {
 			console.log('Local blockstack CORS proxy responded, using local service');
 			BlockstackCommon.login( retUrl, manUrl, 'http://localhost:8888' ).then((url) => {
 				window.location.replace(url);
 			}).catch((err) => {
 				console.error("Error: " + err);
 			});
-		},
-		error: function(err) {
+		}
+
+		else {
 			console.log('Local blockstack CORS roxy not present, falling back on web service');
 			BlockstackCommon.login( retUrl, manUrl ).then((url) => {
 				window.location.replace(url);
@@ -21,5 +24,6 @@ $('#mw-input-blockstacksso').click(function() {
 				console.error("Error: " + err);
 			});
 		}
-	});
+	}, 100);
+
 });

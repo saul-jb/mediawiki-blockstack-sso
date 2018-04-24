f732fc4329223c9185c9e600000f7701f3c62af3
Aranad <aran@organicdesign.co.nz>
2018-04-24 12:28:58 -0300

    use local CORS proxy to test dapp presence

--- modules/blockstacksso.js
+++ modules/blockstacksso.js
@@ -1,12 +1,25 @@
 $('#mw-input-blockstacksso').click(function() {
-	BlockstackCommon.login(
-		mw.config.get('wgServer') + mw.config.get('wgScript') + '?title=' + mw.config.get('wgPageName'),
-		mw.config.get( 'blockstackManifestUrl' ),
-		'http://localhost:8888'
-	).then( (url) => {
-		window.location.replace( url );
-	}).catch( (err) => {
-		console.error( "Error: " + err );
+	var retUrl = mw.config.get('wgServer') + mw.config.get('wgScript') + '?title=' + mw.config.get('wgPageName');
+	var manUrl = mw.config.get( 'blockstackManifestUrl' );
+	$.ajax({
+		type: 'GET',
+		url: 'http://localhost:1337',
+		timeout: 100,
+		success: function(text) {
+			console.log('Local blockstack CORS proxy responded, using local service');
+			BlockstackCommon.login( retUrl, manUrl, 'http://localhost:8888' ).then((url) => {
+				window.location.replace(url);
+			}).catch((err) => {
+				console.error("Error: " + err);
+			});
+		},
+		error: function(err) {
+			console.log('Local blockstack CORS roxy not present, falling back on web service');
+			BlockstackCommon.login( retUrl, manUrl ).then((url) => {
+				window.location.replace(url);
+			}).catch((err) => {
+				console.error("Error: " + err);
+			});
+		}
 	});
-
 });

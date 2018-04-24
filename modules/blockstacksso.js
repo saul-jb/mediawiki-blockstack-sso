cf8a88081daa5fdb1ac0cf8149c12c0c811df6d6
Aranad <aran@organicdesign.co.nz>
2018-04-24 13:33:53 -0300

    dodgy way to do local dapp test

--- modules/blockstacksso.js
+++ modules/blockstacksso.js
@@ -1,13 +1,20 @@
 $(document).ready(function() {
 	$('#mw-input-blockstacksso').click(function() {
+
+		/**
+		 * This image preload is a test of local Blockstack dapp presence
+		 * even though there is a local service on port 1337 that can be tested without CORS trouble,
+		 * it can't get around the "mixed content" problem (requesting http://localhost:1337 from an HTTPS page over XHR).
+		 * An image can be loaded from the dapp service though without these limitations, we can then test after a small
+		 * delay (100ms) whether we have a non-zero height for the image.
+		 */
 		var img = new Image()
 		img.src = 'http://localhost:8888/images/icon-nav-profile.svg';
 		setTimeout(function() {
-			console.log(img.height);
-
 			var retUrl = mw.config.get('wgServer') + mw.config.get('wgScript') + '?title=' + mw.config.get('wgPageName');
 			var manUrl = mw.config.get( 'blockstackManifestUrl' );
 
+			// Image height is non-zero, Blockstack dapp is serving on local port 8888
 			if(img.height > 0) {
 				console.log('Local blockstack CORS proxy responded, using local service');
 				BlockstackCommon.login( retUrl, manUrl, 'http://localhost:8888' ).then((url) => {
@@ -17,6 +24,7 @@ $(document).ready(function() {
 				});
 			}
 
+			// Image height is zero, fallback to web-based Blockstack service
 			else {
 				console.log('Local blockstack CORS roxy not present, falling back on web service');
 				BlockstackCommon.login( retUrl, manUrl ).then((url) => {

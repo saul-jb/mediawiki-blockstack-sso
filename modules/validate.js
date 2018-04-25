623175be60537f42ef13137a80faf640876863e2
Aranad <aran@organicdesign.co.nz>
2018-04-25 18:32:49 -0300

    almost there

--- modules/validate.js
+++ modules/validate.js
@@ -1,15 +1,17 @@
 window.validate = function() {
 	console.log('Validating Blockstack response...');
 	BlockstackCommon.isSignedIn().then(function(userData) {
-		var username = userData.username;
-		var realname = userData.profile.name;
-
 		BlockstackCommon.phpSignIn(userData).then(function(data) {
-			console.log(data);
+			console.log('Blockstack authentication successful');
+			var username = userData.username;
+			var realname = userData.profile.name;
+			var wikiuser = userData.login ? userData.login.username : false;
+
+			// TODO: POST this data to the login form for server-side validation
+
 		}).catch(function(err) {
 			console.error(err.data);
 		});
-
 	}).catch(function(err) {
 		console.error(err.data);
 	});

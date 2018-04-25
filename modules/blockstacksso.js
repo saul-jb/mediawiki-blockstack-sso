c24df7ded30f80f3172decc0b5304e26000e0551
nutmmm <nutmmm@organicdesign.co.nz>
2018-04-26 11:07:22 +1200

    Removed the redundant need to externally check for the blockstack service.

--- modules/blockstacksso.js
+++ modules/blockstacksso.js
@@ -4,44 +4,16 @@
  *
  * This detection is rather difficult because the services are on HTTP, but the site we're logging in to may be on HTTPS
  * in which case a non-HTTPS XHR request will not be allowed.
- * 
+ *
  * To get around this, an image is loaded from the dapp service which doesn't suffer from the "mixed content" restriction.
  * We can then test after a small delay (100ms) whether we have a non-zero height for the image.
  */
-$(document).ready(function() {
-	$('#mw-input-blockstacksso').click(function() {
-
-		// Request the test image from the local dapp service
-		var img = new Image()
-		img.src = 'http://localhost:8888/images/icon-nav-profile.svg';
-
-		// Wait 100ms to give the image time to load and then start the redirect procedure
-		setTimeout(function() {
-			var retUrl = mw.config.get('wgServer')
-				+ mw.config.get('wgScript')
-				+ '&action=blockstack-validate'
-				+ '&token=' + encodeURIComponent( document.userlogin.wpLoginToken.value );
-			var manUrl = mw.config.get( 'blockstackManifestUrl' );
-
-			// Test-image height is non-zero, Blockstack dapp is serving on local port 8888
-			if(img.height > 0) {
-				console.log('Local blockstack CORS proxy responded, using local service');
-				BlockstackCommon.login( retUrl, manUrl, 'http://localhost:8888' ).then((url) => {
-					window.location.replace(url);
-				}).catch((err) => {
-					console.error("Error: " + err);
-				});
-			}
-
-			// Test-image height is zero, fallback to web-based Blockstack service
-			else {
-				console.log('Local blockstack CORS proxy not present, falling back on web service');
-				BlockstackCommon.login( retUrl, manUrl ).then((url) => {
-					window.location.replace(url);
-				}).catch((err) => {
-					console.error("Error: " + err);
-				});
-			}
-		}, 100);
-	});
-});
+ $(document).ready(function() {
+ 	$('#mw-input-blockstacksso').click(function() {
+ 		BlockstackCommon.login( retUrl, manUrl ).then((url) => {
+ 			window.location.replace(url);
+ 		}).catch((err) => {
+ 			console.error("Error: " + err);
+ 		});
+ 	});
+ });

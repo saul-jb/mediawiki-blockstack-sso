11f2ed75ecc73df9dd8f568c4286ac325536c547
Aranad <aran@organicdesign.co.nz>
2018-04-25 15:00:13 -0300

    specify blockstack in submit

--- modules/validate.js
+++ modules/validate.js
@@ -1,6 +1,8 @@
 window.validate = function() {
 	console.log('Validating Blockstack response...');
 	BlockstackCommon.isSignedIn().then(function(userData) {
+		var username = userData.username;
+		var realname = userData.profile.name;
 		console.log(userData);
 		/*
 		var url = window.location.orgin + "/authenticate";
